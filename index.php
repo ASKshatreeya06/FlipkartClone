92f5c9bc6101de16f8da7be3f7ebe34c5192a9cd
Prakash singh <singhprakash545@gmail.com>
2023-01-09 09:18:30 +0530

    header of clone Flipkart
    navbar

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>flipkart |Home</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/utis.css">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <div class="logo">
+                <a href="index.php"><img src="./img/Flipkart-logo.png"></a>
+            </div>
+            <ul>
+                <li><a href="home.php">Home</a> </li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+            <div class="search">
+                <input type="text" name="" id="" placeholder="Search...........">
+                <button class="btn">search</button>
+            </div>
+            <div class="account">
+                <button class="btn1">Log in</button>
+                <button class="btn">Sign Up</button>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <div class="container">
+            <div class="topbar">
+                <ul>
+                    <li><a href=""><img style="height: 58px;" src="./img/phone.png" alt=""></a></li>
+                    <li><a href=""><img style="height: 58px;" src="./img/1.jpg" alt=""></a></li>
+                    <li><a href=""><img style="height: 58px;" src="./img/R.png" alt=""></a></li>
+                </ul>
+            </div>
+        </div>
+    </main>
+    <footer class="flex-all-center">
+        <p>Copyringh &copy; flipkart clone </p>
+    </footer>
+</body>
+
+</html>
